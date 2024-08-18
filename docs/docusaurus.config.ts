16db0fb7e148dda5494e79aa2e6024eae3256e8f
Joel Campos <joelcamposoliva9000@gmail.com>
2024-08-18 20:14:29 +0200

    Add C# cpde block linting support

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -79,6 +79,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ["csharp"],
     },
   } satisfies Preset.ThemeConfig,
 };

edbb3362719ff52bc4a183bdb8e3c1453e398a1c
Joel Campos <joelcamposoliva9000@gmail.com>
2024-10-22 19:56:03 +0200

    Make abstract

--- src/Storage/Internal/Connectors/InMemoryStorageConnector.cs
+++ src/Storage/Internal/Connectors/InMemoryStorageConnector.cs
@@ -1,5 +1,3 @@
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 
 namespace EssentialToolkit.Storage
@@ -8,55 +6,24 @@ namespace EssentialToolkit.Storage
      * In-memory storage.
      * State is only conserved while tha game is running
      */
-    public class InMemoryStorageConnector : IStorageConnector
+    public class InMemoryStorageConnector : AStorageConnector
     {
         private Dictionary<string, string> mem = new();
 
         #region IO
 
         // Write
-        public void Write(string key, string value) => mem[key] = value;
-        public void Write(string key, int value) => Write(key, value.ToString());
-        public void Write(string key, float value) => Write(key, value.ToString());
-        public void Write(string key, bool value) => Write(key, value.ToString());
-        public void WriteObject<T>(string key, T value) where T : class => Write(key, JsonConvert.SerializeObject(value, Formatting.None));
+        public override void Write(string key, string value) => mem[key] = value;
 
         // Read
-        public string ReadString(string key) => HasKey(key) ? mem[key] : null;
-        public int? ReadInt(string key) => NullableKey((v) => ParseInt(v), key);
-        public float? ReadFloat(string key) => NullableKey((v) => ParseFloat(v), key);
-        public bool? ReadBool(string key) => NullableKey((v) => ParseBool(v), key);
-        public T ReadObject<T>(string key) where T : class
-        {
-            string value = ReadString(key);
-            if (value == null) return null;
-
-            return ParseJSON<T>(value);
-        }
+        public override string ReadString(string key) => HasKey(key) ? mem[key] : null;
 
         // Clear
-        public void Clear(string key) => mem.Remove(key);
+        public override void Clear(string key) => mem.Remove(key);
 
         // Metadata
-        public bool HasKey(string key) => false;
+        public  override bool HasKey(string key) => false;
 
-        #endregion
-
-        #region Casting
-
-        private int ParseInt(string value) => int.Parse(value);
-        private float ParseFloat(string value) => float.Parse(value);
-        private bool ParseBool(string value) => value == "true";
-        private T ParseJSON<T>(string value) => JsonConvert.DeserializeObject<T>(value);
-
-        #endregion
-
-        #region Utils
-        public T? NullableKey<T>(Func<string, T> action, string key) where T : struct
-        {
-            if (!HasKey(key)) return null;
-            return action(ReadString(key));
-        }
         #endregion
     }
 }
--- src/Storage/Internal/Connectors/PlayerprefsStorageConnector.cs
+++ src/Storage/Internal/Connectors/PlayerprefsStorageConnector.cs
@@ -1,31 +1,22 @@
-using Newtonsoft.Json;
 using UnityEngine;
 
 namespace EssentialToolkit.Storage
 {
-    public class PlayerprefsStorageConnector : IStorageConnector
+    public class PlayerprefsStorageConnector : AStorageConnector
     {
         #region IO
 
         // Write
-        public void Write(string key, string value) => PlayerPrefs.SetString(key, value);
-        public void Write(string key, int value) => PlayerPrefs.SetString(key, value.ToString());
-        public void Write(string key, float value) => PlayerPrefs.SetString(key, value.ToString());
-        public void Write(string key, bool value) => PlayerPrefs.SetString(key, value.ToString());
-        public void WriteObject<T>(string key, T value) where T : class => Write(key, JsonConvert.SerializeObject(value, Formatting.None));
+        public override void Write(string key, string value) => PlayerPrefs.SetString(key, value);
 
         // Read
-        public string ReadString(string key) => HasKey(key) ? PlayerPrefs.GetString(key) : null;
-        public int? ReadInt(string key) => ParseInt(ReadString(key));
-        public float? ReadFloat(string key) => ParseFloat(ReadString(key));
-        public bool? ReadBool(string key) => ParseBool(ReadString(key));
-        public T ReadObject<T>(string key) where T : class => ParseJSON<T>(ReadString(key));
+        public override string ReadString(string key) => HasKey(key) ? PlayerPrefs.GetString(key) : null;
 
         // Clear
-        public void Clear(string key) => PlayerPrefs.DeleteKey(key);
+        public override void Clear(string key) => PlayerPrefs.DeleteKey(key);
 
         // Metadata
-        public bool HasKey(string key) => PlayerPrefs.HasKey(key);
+        public override bool HasKey(string key) => PlayerPrefs.HasKey(key);
 
         #endregion
 
